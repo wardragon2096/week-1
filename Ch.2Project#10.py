b47e35950400d96960b0c87d88763ab3e4e3e97c
wardragon2096 <73207524+wardragon2096@users.noreply.github.com>
2020-11-21 21:56:51 -0600

    Add files via upload

---
+++ Ch.2Project#10.py
@@ -0,0 +1,5 @@
+hourly_wages = float(input("Enter hourly wage: "))
+regular_hours = float(input("Enter total number of regular hours: "))
+overtime_hours = float(input("Enter overtime hours: "))
+weekly_pay = (hourly_wages * regular_hours) + (1.5 * hourly_wages * overtime_hours)
+print ("Total weekly pay:", weekly_pay)
---
+++ Ch.2Project#9.py
@@ -0,0 +1,3 @@
+km = float(input("Enter the number of Kilometers: "))
+nautical_miles = (km * 90 * 60) /10000
+print (km, "Kilometers =", nautical_miles)
